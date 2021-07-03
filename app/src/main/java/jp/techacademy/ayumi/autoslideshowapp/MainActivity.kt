ac47cbc41b6626f6358e924583726e2639f6d058
ayumi-tulips <snowcat.kuroneko@gmail.com>
2021-07-03 19:23:59 +0900

    aa

--- app/src/main/java/jp/techacademy/ayumi/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/ayumi/autoslideshowapp/MainActivity.kt
@@ -8,12 +8,23 @@ import android.os.Build
 import android.util.Log
 import android.provider.MediaStore
 import android.content.ContentUris
+import android.database.Cursor
+import android.os.Handler
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private val PERMISSIONS_REQUEST_CODE = 100
 
+        //タイマー
+    private val TIMER_SEC = 2000
+
+    private var mTimer: Timer? = null
+    private val mHandler = Handler()
+    private var mCursor: Cursor? = null
+
+    private var runflg = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,16 +34,60 @@ class MainActivity : AppCompatActivity() {
             // パーミッションの許可状態を確認する
             if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
                 // 許可されている
-                getContentsInfo()
+               mCursor = getCursor()
             } else {
                 // 許可されていないので許可ダイアログを表示する
                 requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSIONS_REQUEST_CODE)
             }
             // Android 5系以下の場合
         } else {
-            getContentsInfo()
+            mCursor = getCursor()
         }
     }
+
+    //再生停止ボタン
+    buttonRun.setOnClickListener
+    {
+        if (runflg) {
+            mTimer?.cancel()
+            mTimer = Timer()
+            mTimer!!.Schdule(object : TimerTask) {
+                override fun run() {
+                    mHandler.post {
+                        if (mCursor != null)
+                            showImage()
+                        //最後まで来たら最初から
+                        if (!mCursor!!.moveToNext()) mCursor!!.moveToFirst()
+                    }
+                }
+            }
+        }, TIMER_SEC.toLong(),TIMER_SEC.toLong())
+    }
+}
+    runflg = !runflg
+    setBtnEnable()
+}
+
+//戻るボタン
+buttonBack.setOnclickListener {
+    it:View!
+    if (mCursor != null) {
+        if (!mCursor!!.moveToprevious()) mCursor!!.moveToLast()
+        showImage()
+    }
+}
+
+//進ボタン
+buttonNext.setOnclickListener {
+    it:view!
+    if (mCursor != null) {
+        if (!mCursor!!.moveToPrevious()) mCursor!!.moveToLast()
+        showImage()
+    }
+}
+    setBtnEnable()
+}
+
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         when (requestCode) {
             PERMISSIONS_REQUEST_CODE ->
@@ -53,17 +108,28 @@ class MainActivity : AppCompatActivity() {
             null // ソート (nullソートなし）
         )
 
-        if (cursor!!.moveToFirst()) {
-            do {
-                // indexからIDを取得し、そのIDから画像のURIを取得する
-                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-                val id = cursor.getLong(fieldIndex)
-                val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+        return if (cursor!!.moveToFirst()) {
+            cursor
+        } else{
+            cursor.close()
+            null
+       }
+}
 
-                Log.d("ANDROID", "URI : " + imageUri.toString())
-            } while (cursor.moveToNext())
-        }
-        cursor.close()
-
+//immageの表示
+private fun showImage()
+    if(mCusor != null){
+        val  fieldIndex = mCursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+        val id = mCursor!!.getLong(fieldIndex)
+        val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,id)
+        imageView.setImageURL(imageUri)
     }
 }
+        //ボタンの表示有効有効無効を切り替える
+private fun setBtnEnable() {
+            buttonRun.text = if (runflg) "停止" else "再生"
+            buttonBack.isEnabled = !runflg
+            buttonNext.isEnabled = !runflg
+        }
+}
+
